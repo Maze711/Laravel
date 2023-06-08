38efc54b7003d60bac056ce8b683e98933b65ce4
Maze711 <badlonmazeclarion@gmail.com>
2023-06-08 11:26:09 -0700

    Registration Done

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class FormController extends Controller
+{
+    public function index()
+    {
+        return view('form');
+    }
+ 
+    public function store(Request $request)
+    {
+         
+        $validatedData = $request->validate([
+          'name' => 'required',
+          'email' => 'required|unique:users|max:255',
+          'password' => 'required',
+        ]);
+ 
+        $user = new User;
+ 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+ 
+        $user->save();
+ 
+        return redirect('form')->with('status', 'Form Data Has Been Inserted');
+ 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FormController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// TO-DO LIST Web
+Route::get('/tasks', 'App\Http\Controllers\TaskController@index');
+Route::post('/tasks', 'App\Http\Controllers\TaskController@store');
+Route::get('/tasks/{id}/edit', 'App\Http\Controllers\TaskController@edit');
+Route::put('/tasks/{id}', 'App\Http\Controllers\TaskController@update');
+Route::delete('/tasks/{id}', 'App\Http\Controllers\TaskController@destroy');
+
+Route::get('form', [FormController::class, 'index']);
+Route::post('store-form', [FormController::class, 'store']);
